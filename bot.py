c2448d504903d8477113dc3fcc6d19fb3a54780c
Anime Sparta <suryagupta1928@gmail.com>
2024-06-08 22:23:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Beesmileee")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/MuGiWaRaNoLuFFY23")
         self.LOGGER(__name__).info(f""" \n\n       
 ░█████╗░███╗░░██╗██╗███╗░░░███╗███████╗██╗░░██╗██╗░░░██╗███╗░░██╗████████╗███████╗██████╗░░██████╗
 ██╔══██╗████╗░██║██║████╗░████║██╔════╝██║░░██║██║░░░██║████╗░██║╚══██╔══╝██╔════╝██╔══██╗██╔════╝
